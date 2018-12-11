7124ec443f605f04a9b9e406b72a70e7eef31c4f
nitanmarcel <nitan.marcel@gmail.com>
2018-12-11 13:48:57 +0200

    Added proxy support! args: --host --port

--- tg_userbot/__init__.py
+++ tg_userbot/__init__.py
@@ -1,5 +1,7 @@
 from ._version import __version__
 from telethon import TelegramClient, sync
+from argparse import ArgumentParser
+import socks
 import sys
 import os
 import logging
@@ -16,8 +18,14 @@ if sys.version_info[0] < 3 or sys.version_info[1] < 6:
     LOGGER.error("You MUST have a python version of at least 3.6! Multiple features depend on this. Bot quitting.")
     quit(1)
 
+parser = ArgumentParser()
+parser.add_argument('--host', help='a valid host')
+parser.add_argument('--port', help='a valid port')
 
-
+args = parser.parse_args()
+host = args.host
+port = args.port
+proxy = None
 
 dotenv.load_dotenv('config.env')
 
@@ -30,8 +38,10 @@ DISABLED = []
 if not DB_URI:
     DISABLED.append('notes')
 
+if host and port:
+    proxy = (socks.SOCKS5, host, int(port))
 
 version = f'{__version__.major}.{__version__.minor}.{__version__.micro}'
 
-client = TelegramClient('tg_userbot', APP_ID, APP_HASH, app_version=version)
+client = TelegramClient('tg_userbot', APP_ID, APP_HASH, proxy=proxy, app_version=version)
 
