6af62fa56260bed53f0dca37194a2374f0da51fa
nitanmarcel <nitan.marcel@gmail.com>
2019-01-15 23:21:22 +0200

    Added .github command

---
+++ tg_userbot/modules/github.py
@@ -0,0 +1,50 @@
+from telethon import events
+from tg_userbot import client
+import requests
+
+URL = 'https://api.github.com/users/'
+
+
+@client.on(events.NewMessage(outgoing=True, pattern=r"\.github (\w*)"))
+async def github(e):
+
+
+    request = requests.get(URL + e.pattern_match.group(1))
+
+    if request.status_code == 404:
+        await e.reply('`' + e.pattern_match.group(1) + ' not found`')
+        return
+
+
+    result = request.json()
+
+    url = result.get('html_url', None)
+    name = result.get('name', None)
+    company = result.get('company', None)
+    bio = result.get('bio', None)
+    created_at = result.get('created_at', 'Not Found')
+
+    REPLY = f"""
+    GitHub Info for `{e.pattern_match.group(1)}`
+
+    Username: `{name}`
+    Bio: `{bio}`
+    URL: `{url}`
+    Company: `{company}`
+    Created at: `{created_at}`
+    """
+
+    request = requests.get(result.get('repos_url', None))
+    if request.status_code == 404:
+        await e.edit(REPLY)
+        return
+
+    result = request.json()
+    chat = await e.get_chat()
+
+    REPLY += '\nRepos: \n\n'
+
+    for nr in range(len(result)):
+        REPLY += '  [{}]({})\n'.format(result[nr].get('name', None), result[nr].get('html_url', None))
+
+    await client.send_message(chat.id, message=REPLY, reply_to= e.id, link_preview=False)
